930cff2a6f4fffd371f5d3c331cd84e7995accd6
test app <test_app@mail.md>
2020-01-19 20:29:01 +0200

    New updates

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,37 @@
+class ArticlesController < ApplicationController
+
+
+def new
+	@article = Article.new
+end
+
+def show
+	@article = Article.find(params[:id])
+end
+
+
+def index
+
+end
+
+
+def create
+
+	@article = Article.new(article_params)
+	if @article.save
+		flash[:notice] = 'The article was created'
+		redirect_to article_path(@article)
+	else
+		render 'new'
+	end	
+end
+
+
+private
+
+def article_params
+
+	params.require(:article).permit(:title, :description)
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'welcome#home'
   get '/about', to: 'welcome#about'
+  resources :articles
 end
