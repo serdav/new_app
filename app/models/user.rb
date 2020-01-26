852123e74684988bcf99419b8c5a8983e16fa289
test app <test_app@mail.md>
2020-01-26 18:29:55 +0200

    new assoc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-
+has_many :articles
+before_save { self.email = email.downcase }
+validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximun: 25 }
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, presence: true, uniqueness: true, length: { maximun: 50 }, format: VALID_EMAIL_REGEX
 end
